ac4ced2fdd5a3b5f75c30fea79559740344eae09
Krystian Gwizdala <gwizdala.kr@gmail.com>
2016-05-11 12:44:56 +0200

    Save logs to file

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var morgan = require('morgan');
+var fs = require('fs');
 
 var config = require('./app/config');
 
 app.use(cors());
 app.set('port', (process.env.PORT || config.DEFAULT_PORT));
 app.use(express.static(__dirname + '/public'));
+
+var accessLogStream = fs.createWriteStream(__dirname + '/logfile.log', { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
 
 var server = app.listen(app.get('port'), function() {
   console.log('App is running at port:' + app.get('port'));
